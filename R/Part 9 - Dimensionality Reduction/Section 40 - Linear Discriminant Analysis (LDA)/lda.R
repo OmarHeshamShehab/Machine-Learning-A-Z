25ab6ead8195cce342d8517a3e5292bcdd7a8619
Omar Hesham <omar.hesham.shehab@gmail.com>
2024-01-01 21:30:46 +0200

    added

---
+++ R/Part 9 - Dimensionality Reduction/Section 40 - Linear Discriminant Analysis (LDA)/lda.R
@@ -0,0 +1,82 @@
+setwd("E:/Courses/Machine-Learning-A-Z/R/Part 9 - Dimensionality Reduction/Section 40 - Linear Discriminant Analysis (LDA)") # nolint
+# LDA
+
+# Importing the dataset
+dataset <- read.csv("Wine.csv")
+
+# Splitting the dataset into the Training set and Test set
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-14] <- scale(training_set[-14])
+test_set[-14] <- scale(test_set[-14])
+
+# Applying LDA
+library(MASS)
+lda <- lda(formula = Customer_Segment ~ ., data = training_set)
+training_set <- as.data.frame(predict(lda, training_set))
+training_set <- training_set[c(5, 6, 1)]
+test_set <- as.data.frame(predict(lda, test_set))
+test_set <- test_set[c(5, 6, 1)]
+
+# Fitting SVM to the Training set
+
+library(e1071)
+classifier <- svm(
+  formula = class ~ .,
+  data = training_set,
+  type = "C-classification",
+  kernel = "linear"
+)
+
+# Predicting the Test set results
+y_pred <- predict(classifier, newdata = test_set[-3])
+
+# Making the Confusion Matrix
+cm <- table(test_set[, 3], y_pred)
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set <- training_set
+x1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+x2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(x1, x2)
+colnames(grid_set) <- c("x.LD1", "x.LD2")
+y_grid <- predict(classifier, newdata = grid_set)
+plot(set[, -3],
+  main = "SVM (Training set)",
+  xlab = "LD1", ylab = "LD2",
+  xlim = range(x1), ylim = range(x2)
+)
+contour(x1, x2, matrix(as.numeric(y_grid), length(x1), length(x2)), add = TRUE)
+points(grid_set, pch = ".", col = ifelse(y_grid == 2,
+  "deepskyblue", ifelse(y_grid == 1, "springgreen3", "tomato")
+))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2,
+  "blue3", ifelse(set[, 3] == 1, "green4", "red3")
+))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set <- test_set
+x1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+x2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(x1, x2)
+colnames(grid_set) <- c("x.LD1", "x.LD2")
+y_grid <- predict(classifier, newdata = grid_set)
+plot(set[, -3],
+  main = "SVM (Test set)",
+  xlab = "LD1", ylab = "LD2",
+  xlim = range(x1), ylim = range(x2)
+)
+contour(x1, x2, matrix(as.numeric(y_grid), length(x1), length(x2)), add = TRUE)
+points(grid_set, pch = ".", col = ifelse(y_grid == 2,
+  "deepskyblue", ifelse(y_grid == 1, "springgreen3", "tomato")
+))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2,
+  "blue3", ifelse(set[, 3] == 1, "green4", "red3")
+))
